ad5493825deff18883500bd054f78193bb88bb59
Gro <70800891+gro-86@users.noreply.github.com>
2023-09-20 19:15:58 +0200

    Cambios 3

--- msalumnos/src/main/java/edu/cta/academy/entity/Alumno.java
+++ msalumnos/src/main/java/edu/cta/academy/entity/Alumno.java
@@ -1,10 +1,34 @@
 package edu.cta.academy.entity;
 
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.PrePersist;
 import javax.persistence.Table;
 
-@Entity
+@Entity //Tabla asociada a una BD
 @Table(name = "alumnos")
 public class Alumno {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY) //Autoincremento de ID en Mysql
+	private Long id;
+	
+	private String nombre;
+	private String apellido;
+	private String email;
+	private int edad;
+	
+	@Column(name="creado_en")
+	private LocalDateTime creadoEn;
+	
+	@PrePersist //Antes de que se inserte un alumno, se ejecuta este método
+	private void generarFechaCreacion() {
+		this.creadoEn= LocalDateTime.now(); //Fecha y hora actual
+	};
 
 }
