c74403f75892877a211ab79a4d88b57faedbaa44
Gro <70800891+gro-86@users.noreply.github.com>
2023-09-20 20:46:27 +0200

    cambio final

--- msalumnos/src/main/java/edu/cta/academy/controller/AlumnoController.java
+++ msalumnos/src/main/java/edu/cta/academy/controller/AlumnoController.java
@@ -1,12 +1,39 @@
 package edu.cta.academy.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import edu.cta.academy.entity.Alumno;
 
 /**
  * Recibe y contesta a las peticiones de los clientes
  * */
 
 @RestController
+@CrossOrigin
+@RequestMapping("/alumno") //Le dices al servidor que todo lo que sea /alumno, es para esta clase
 public class AlumnoController {
+	
+	//CREATE
+	
+	//DELETE
+	
+	//GETALL
+	@GetMapping //http://localhost:8085/alumno
+	public ResponseEntity<?>listarAlumnos(){
+		// ? Devuelve cualquier cosa. En realidad es un iterable
+		//ResponseEntity representa el mensaje HTTP de respuesta
+		Iterable<Alumno> ita = null;
+		ResponseEntity<?> responseEntity = null;
+		
+		return responseEntity;
+	}
+	
+	//GETById
+	
+	//PUT
 
 }
--- msalumnos/src/main/java/edu/cta/academy/service/AlumnoServiceImpl.java
+++ msalumnos/src/main/java/edu/cta/academy/service/AlumnoServiceImpl.java
@@ -4,12 +4,14 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import edu.cta.academy.entity.Alumno;
 import edu.cta.academy.repository.AlumnoRepository;
 
 /**
  * Realiza las tareas de aplicacion
+ * Transactional implementa un try catch
  * */
 
 @Service
@@ -20,34 +22,38 @@ public class AlumnoServiceImpl implements AlumnoService{
 
 	//GET
 	@Override
+	@Transactional(readOnly=true) //Optimizo para no bloquear la tabla y permitir el acceso concurrente "lectura sucia".
 	public Iterable<Alumno> consultarTodos() {
 		return this.alumnoRepository.findAll();
 	}
 
 	//GETBYID
 	@Override
+	@Transactional(readOnly=true)
 	public Optional<Alumno> consultarPorId(Long id) {	
 		return this.alumnoRepository.findById(id);
 	}
 
 	//DELETE
 	@Override
+	@Transactional
 	public void borrarAlumnoPorId(Long id) {
 		this.alumnoRepository.deleteById(id);
 	}
 	
 	//SAVE
 	@Override
+	@Transactional
 	public Alumno altaAlumno(Alumno alumno) {
 		return	this.alumnoRepository.save(alumno);
 	}
 
 	//PUT
 	@Override
+	@Transactional
 	public Optional<Alumno> modificarPorId(Alumno alumno, Long id) {
 		
 		 Optional <Alumno> alumnoNew = alumnoRepository.findById(id);
-		 
 		 
 		 return alumnoNew;
 	}
